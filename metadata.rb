096ddc541d0bc380a8c08a223f7a4254d5cd65c1
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-04-19 13:05:48 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Install and configure dnsmasq'
 long_description ::IO.read(::File.join(::File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '2.1.0'
 
 scm_url = 'https://github.com/aspyatkin/localdns-cookbook'
 source_url scm_url if respond_to?(:source_url)
 issues_url "#{scm_url}/issues" if respond_to?(:issues_url)
 
-depends 'line', '~> 2.2.0'
+depends 'line', '>= 2.3.0'
 
 supports 'ubuntu'
